b36284c2a13e9acda746b6c9a92610c6fc13cfdc
Lars van de Kerkhof <lars@permanentmarkers.nl>
2019-09-10 15:59:21 +0200

    Make sure that the cache gets populated at the original requested filename!

--- seo/ville/sorl.py
+++ seo/ville/sorl.py
@@ -1,14 +1,26 @@
 from os import path
 from sorl.thumbnail.base import ThumbnailBackend
 from sorl.thumbnail.conf import settings
+from sorl.thumbnail import default
 from .slugger import SEOStorage
 
 
 class SEOThumbnailBackend(ThumbnailBackend):
     def _get_thumbnail_filename(self, source, geometry_string, options):
         name = path.basename(source.name)
+        dirname = path.dirname(source.name)
         base, ext = path.splitext(name)
-        return "%s.%s%s" % (base, geometry_string, ext)
+        return path.join(dirname, "%s.%s%s" % (base, geometry_string, ext))
+
+    def _create_thumbnail(self, source_image, geometry_string, options, thumbnail):
+        key_at_start = thumbnail.key
+        super()._create_thumbnail(source_image, geometry_string, options, thumbnail)
+        key_at_end = thumbnail.key
+
+        # make sure to get cache hits on the original requested filename, not the
+        # filename returned by the file storage backend.
+        if key_at_start != key_at_end:
+            default.kvstore._set(key_at_start, thumbnail)
 
 
 class SEOThumbnailStorage(SEOStorage):
