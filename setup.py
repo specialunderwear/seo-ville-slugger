5706bea04f7ffc83c7d632ce481461c024e6a2bd
Lars van de Kerkhof <lars@permanentmarkers.nl>
2020-01-17 14:59:30 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
 """
+Just One of those things
+------------------------
+
 Seo ville slugger will solve the problems you are having with django, media
 files and filenames.
 
@@ -12,7 +15,51 @@ There are 2 kinds of people that are not happy with that.
 2. The SEO people your customer has hired.
 
 This storage backend stores files named after their hash value and adds a symlink
-with the original name. No duplication and you SEO your heart out.
+with the original name. No duplication and you can SEO your heart out.
+
+Media file and pagespeed mobile score
+=====================================
+
+Ideally, you would want to have long cache expiry headers for your site’s uploaded files,
+Just like you’ve got with your static files.
+That usually poses a problem in conjunction with the pretty urls.
+It is safe to put long expiry headers when using SEOStorage, because the files
+are distributed over folders named after a tiny piece of the hash.
+Obviously this small hash could lead to collisions, so the storage backend will prevent
+it as follows:
+
+In the rare case we encounter two different files with exactly the same filename, and they end up in the same folder
+due to hash collision, the new file will not get the pretty SEO url. Instead the full hashed filename
+will be used instead. In these rare cases you might get a SEO unfriendly url.
+
+We can’t worry about that can we? Phat phat chance anyone will notice!
+
+Usage
+=====
+
+You can use the storage backend on a global level by adding the following to
+your django settings::
+
+    DEFAULT_FILE_STORAGE = 'seo.ville.slugger.SEOStorage'
+
+Sorl thumbnail
+==============
+
+Suppose you want sorl thumbnail to also have nice urls based on the original
+filename?::
+
+    THUMBNAIL_BACKEND = 'seo.ville.sorl.SEOThumbnailBackend'
+    THUMBNAIL_STORAGE  = 'seo.ville.sorl.SEOThumbnailStorage'
+
+With sorl you can pass the slug as a parameter to the thumbnail tag::
+
+    {% thumbnail image "330x450" upscale=True slug=slug as thumb %}
+
+Settings
+========
+
+``HASH_BLOCKSIZE`` change this if you think hashing of the file use too much or not enough memory
+``HASH_DIRNAME_SLICE_SIZE`` change this if you find there are too many collisions.
 """
 from setuptools import setup, find_packages
 
@@ -25,7 +72,7 @@ setup(
     name='seo-ville-slugger',
     # extract version from module.
     version=__version__,
-    description="One of those things",
+    description="This storage backend stores files named after their hash value and adds a symlink with the original name. No duplication and you can SEO your heart out.",
     long_description=__doc__,
     classifiers=[],
     keywords='',
